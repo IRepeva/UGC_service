5d1ba7fc44b10035e3c6b32132c8208c38e84275
Irina Repeva <repeva.i@panoramik.ru>
2022-12-07 17:42:47 +0000

    Add data generation

---
+++ db_research/data_generator.py
@@ -0,0 +1,20 @@
+from db_research.data.models import BaseDataClass
+from db_research.settings import base_settings
+
+
+class BaseDataGenerator:
+    def __init__(self, data_cls: BaseDataClass,
+                 batch_count: int = base_settings.batch_count,
+                 batch_size: int = base_settings.batch_size):
+        self.data_cls = data_cls
+        self.batch_size = batch_size
+        self.batch_count = batch_count
+
+    def generate_batch(self, batch_size: int):
+        return [self.data_cls.generate_row() for _ in range(batch_size)]
+
+    def generate_data(self):
+        return (
+            self.generate_batch(self.batch_size) for _ in
+            range(self.batch_count)
+        )
---
+++ db_research/utils.py
@@ -0,0 +1,58 @@
+import datetime
+import functools
+import random
+import string
+import time
+
+from db_research.settings import base_settings
+
+NOW = '01/12/2022 19:03:43'
+
+USERS_COUNT = 1000
+USER_IDS = [str(i) for i in range(USERS_COUNT)]
+
+MOVIES_COUNT = 1000
+MOVIE_IDS = [str(i) for i in range(MOVIES_COUNT)]
+
+
+def generate_random_string(char_num=16):
+    alphabet = string.ascii_letters + string.digits
+    return ''.join(random.choice(alphabet) for _ in range(char_num))
+
+
+def get_random_user(amount=len(USER_IDS)):
+    return random.choice(USER_IDS[:amount])
+
+
+def get_random_movie(amount=len(MOVIE_IDS)):
+    return random.choice(MOVIE_IDS[:amount])
+
+
+def get_random_date():
+    now_datetime = datetime.datetime.strptime(NOW, "%d/%m/%Y %H:%M:%S")
+    delta = 60*24*365*7
+    random_delta_minutes = datetime.timedelta(minutes=random.randint(0, delta))
+    return now_datetime - random_delta_minutes
+
+
+def benchmark(iterations: int = base_settings.iterations_count):
+    def wrapper(func):
+        @functools.wraps(func)
+        def inner(*args, **kwargs):
+            times = []
+            for _ in range(iterations):
+                start_time = time.perf_counter()
+                func(*args, **kwargs)
+                end_time = time.perf_counter()
+                times.append(end_time - start_time)
+
+            total_time = sum(times)
+            avg_time = total_time / iterations
+
+            print(f"Query: {args[0].name}")
+            print(f"Number of iterations: {iterations}")
+            print(f"Average run time: {avg_time:.4f} sec \n")
+
+        return inner
+
+    return wrapper
