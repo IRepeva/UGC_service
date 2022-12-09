1c4c96c9c1dcd7020c2943c934060376896302fb
Irina Repeva <repeva.i@panoramik.ru>
2022-12-09 19:28:17 +0000

    Add service for Review

--- ugc_service_mongo/src/api/v1/films.py
+++ ugc_service_mongo/src/api/v1/films.py
@@ -1,13 +1,15 @@
 from collections import namedtuple
-from collections import namedtuple
 from http import HTTPStatus
+from typing import List
 
 from fastapi import APIRouter, Depends, HTTPException
 
 from src.models.film import (
-    FilmRating, FilmVote, FilmVotePost
+    FilmRating, FilmVote, FilmVotePost, ReviewLike, ReviewLikePost, FilmReview,
+    FilmReviewPost, FilmReviewDetails
 )
 from src.services.like import LikeService, get_like_service
+from src.services.review import ReviewService, get_review_service
 from src.utils.authentication import get_token_payload, security
 
 router = APIRouter()
@@ -86,5 +88,121 @@ async def delete_film_vote(
             status_code=HTTPStatus.NOT_FOUND,
             detail=f'The error occurred during deleting '
                    f'the movie {film_id} rating'
+        )
+    return result
+
+
+@router.get('/{film_id}/reviews',
+            response_model=List[FilmReviewDetails],
+            summary="Get movie reviews")
+async def film_reviews(
+        film_id: str,
+        sort: str | None = None,
+        review_service: ReviewService = Depends(get_review_service),
+        token: namedtuple = Depends(security)
+) -> List[FilmReviewDetails]:
+    token_payload = get_token_payload(token.credentials)
+    if not (user_id := token_payload.get('user_id')):
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
+
+    reviews = await review_service.get_film_reviews(user_id, film_id, sort)
+    if not reviews:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
+
+    return reviews
+
+
+@router.post("/{film_id}/review", response_model=FilmReview,
+             summary='Upsert movie review')
+async def review_film(
+        film_id: str,
+        film_review: FilmReviewPost,
+        review_service: ReviewService = Depends(get_review_service),
+        token: namedtuple = Depends(security)
+) -> FilmReview:
+    token_payload = get_token_payload(token.credentials)
+    if not (user_id := token_payload.get('user_id')):
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
+
+    result = await review_service.review_film(
+        user_id=user_id,
+        movie_id=film_id,
+        text=film_review.text
+    )
+    if not result:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f'The error occurred during rating the movie {film_id}'
+        )
+    return result
+
+
+@router.delete("/{film_id}/review", response_model=FilmReview,
+               summary='Delete movie review')
+async def review_film(
+        film_id: str,
+        review_service: ReviewService = Depends(get_review_service),
+        token: namedtuple = Depends(security)) -> FilmReview:
+    token_payload = get_token_payload(token.credentials)
+    if not (user_id := token_payload.get('user_id')):
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
+
+    result = await review_service.delete_film_review(
+        user_id=user_id,
+        movie_id=film_id
+    )
+    if not result:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f'The error occurred during rating the movie {film_id}'
+        )
+    return result
+
+
+@router.post("/{film_id}/{review_id}", response_model=ReviewLike,
+             summary='Rate movie review')
+async def review_film(
+        film_id: str,
+        review_id: str,
+        like_review: ReviewLikePost,
+        review_service: ReviewService = Depends(get_review_service),
+        token: namedtuple = Depends(security)) -> ReviewLike:
+    token_payload = get_token_payload(token.credentials)
+    if not (user_id := token_payload.get('user_id')):
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
+
+    result = await review_service.like_review(
+        user_id=user_id,
+        review_id=review_id,
+        rating=like_review.rating
+    )
+    if not result:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f'The error occurred during rating the review {review_id}'
+        )
+    return result
+
+
+@router.delete("/{film_id}/{review_id}", response_model=ReviewLike,
+               summary='Delete review like')
+async def review_film(
+        film_id: str,
+        review_id: str,
+        review_service: ReviewService = Depends(get_review_service),
+        token: namedtuple = Depends(security)) -> ReviewLike:
+    token_payload = get_token_payload(token.credentials)
+    if not (user_id := token_payload.get('user_id')):
+        raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
+
+    result = await review_service.delete_review_like(
+        user_id=user_id,
+        review_id=review_id
+    )
+    if not result:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail=f'The error occurred during deleting '
+                   f'the review {review_id} rating'
         )
     return result
---
+++ ugc_service_mongo/src/services/review.py
@@ -0,0 +1,147 @@
+import datetime
+import random
+from functools import lru_cache
+from typing import Optional
+
+from faker import Faker
+from fastapi import Depends
+from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import ReturnDocument
+
+from src.db.mongo import get_mongo
+from src.models.film import FilmReview, FilmReviewDetails, ReviewLike
+from src.services.base import BaseService
+from src.services.like import LikeService
+from src.utils.data_generation import (
+    get_random_user, get_random_movie, get_random_date
+)
+from src.utils.parser import parse_sort
+
+faker = Faker()
+
+
+class ReviewService(BaseService):
+    COLLECTION = 'reviews'
+    DEPENDENT_COLLECTION = 'review_likes'
+
+    async def get_film_reviews(self, user_id: Optional[str],
+                               movie_id: str, sort: str):
+        aggregate_pipeline = [
+            {'$match': {'movie_id': movie_id}},
+            {
+                '$lookup': {
+                    'from': self.DEPENDENT_COLLECTION,
+                    'localField': 'review_id',
+                    'foreignField': 'review_id',
+                    'as': 'review_likes'
+                }
+            }
+        ]
+        if sort:
+            aggregate_pipeline.append({'$sort': parse_sort(sort)})
+
+        reviews_cursor = self.collection.aggregate(aggregate_pipeline)
+        if not reviews_cursor:
+            return
+
+        likes_collection = self.database.get_collection(LikeService.COLLECTION)
+
+        user_rating = await likes_collection.find_one(
+            {'movie_id': movie_id, 'user_id': user_id}
+        )
+        rating = (
+            user_rating['rating'] if user_rating else
+            await LikeService.get_average_rating(likes_collection, movie_id)
+        )
+        reviews = await reviews_cursor.to_list(length=100)
+        print(f"review likes: {reviews[0].get('review_likes')}")
+        return [
+            FilmReviewDetails(
+                user_id=review['user_id'],
+                movie_id=movie_id,
+                review_id=review['review_id'],
+                text=review.get('text'),
+                date=review.get('date'),
+                rating=round(rating, 2),
+                review_likes=[d['rating'] for d in review.get('review_likes')]
+            )
+            for review in reviews
+        ]
+
+    async def review_film(self, user_id: str, movie_id: str, text: str):
+        new_review = await self.collection.find_one_and_replace(
+            {'user_id': user_id, 'movie_id': movie_id},
+            {
+                'review_id': self.COLLECTION + ':' + user_id + ':' + movie_id,
+                'user_id': user_id,
+                'movie_id': movie_id,
+                'text': text,
+                'date': datetime.datetime.now()
+            },
+            projection={"_id": False},
+            return_document=ReturnDocument.AFTER,
+            upsert=True,
+        )
+
+        return FilmReview.parse_obj(new_review) if new_review else None
+
+    async def delete_film_review(self, user_id: str, movie_id: str):
+        deleted_review = await self.collection.find_one_and_delete(
+            {'user_id': user_id, 'movie_id': movie_id},
+            projection={"_id": False},
+        )
+        return FilmReview.parse_obj(deleted_review) if deleted_review else None
+
+    async def like_review(self, user_id: str, review_id: str, rating: int):
+        review = await self.collection.find_one({'review_id': review_id})
+        if not review:
+            return
+
+        review_likes = self.database.get_collection(self.DEPENDENT_COLLECTION)
+        new_review_like = await review_likes.find_one_and_replace(
+            {'user_id': user_id, 'review_id': review_id},
+            {'user_id': user_id, 'review_id': review_id, 'rating': rating},
+            projection={"_id": False},
+            return_document=ReturnDocument.AFTER,
+            upsert=True,
+        )
+        return ReviewLike.parse_obj(
+            new_review_like) if new_review_like else None
+
+    async def delete_review_like(self, user_id: str, review_id: str):
+        review = await self.collection.find_one({'review_id': review_id})
+        if not review:
+            return
+
+        review_likes = self.database.get_collection(self.DEPENDENT_COLLECTION)
+        deleted = await review_likes.find_one_and_delete(
+            {'user_id': user_id, 'review_id': review_id},
+            projection={"_id": False}
+        )
+        return ReviewLike.parse_obj(deleted) if deleted else None
+
+    @classmethod
+    def generate_row(cls):
+        user_id, movie_id = get_random_user(), get_random_movie()
+        return {
+            'review_id': cls.COLLECTION + ':' + user_id + ':' + movie_id,
+            'user_id': user_id,
+            'movie_id': movie_id,
+            'text': faker.text(),
+            'date': get_random_date(),
+        }
+
+    @classmethod
+    def generate_dependent_row(cls):
+        user_id, movie_id = get_random_user(), get_random_movie()
+        return {
+            'user_id': get_random_user(),
+            'review_id': cls.COLLECTION + ':' + user_id + ':' + movie_id,
+            'rating': random.randint(0, 1)
+        }
+
+
+@lru_cache()
+def get_review_service(
+        mongo: AsyncIOMotorClient = Depends(get_mongo)) -> ReviewService:
+    return ReviewService(mongo)
